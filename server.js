de972f884c777ff05f12230c1489b656a2f10862
Mukul Jaggi <mkujaggi@gmail.com>
2018-04-13 15:48:07 +0530

    setup start script and herou port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
-
+const port=process.env.PORT || 3000;
 var app=express();
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine','hbs');
@@ -55,6 +55,6 @@ app.get('/bad',(req,res)=>{
         error:'bad request'
     })
 })
-app.listen(3000,()=>{
-    console.log('Server is up and running.');
+app.listen(port,()=>{
+    console.log(`Server is up and running. on port ${port}`);
 });
